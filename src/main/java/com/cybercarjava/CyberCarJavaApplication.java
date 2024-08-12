0f646d4205a920cc9949ad97f187d96f50a700c0
rim109 <fla7722@naver.com>
2024-08-12 10:10:00 +0900

    feat:first commit

---
+++ src/main/java/com/cybercarjava/CyberCarJavaApplication.java
@@ -0,0 +1,13 @@
+package com.cybercarjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberCarJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberCarJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cybercarjava/CyberCarJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybercarjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberCarJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
